4c032c8abf2387d06f36e2b884bf8668486b45e1
Διονύσιος Κοντοζίδης <32637139+dionisiskon@users.noreply.github.com>
2017-12-05 21:24:49 +0200

    Update state1.js

--- state1.js
+++ state1.js
@@ -1,5 +1,25 @@
 var state1= {
 	preload: function() {
+	this.load.audio('maintheme','audio/maintheme1.mp3');
+	this.load.audio('stomp','audio/stomp.mp3');
+ 	this.load.audio('jump','audio/jump.mp3');
+ 	this.load.audio('coinsc','audio/coin.mp3');
+ 	this.load.audio('death','audio/death.mp3');
+ 	this.load.image('pic','assets/coin1.png');
+ 	this.load.image('live','assets/mariolive.png');
+ 	this.load.image('gpic','assets/goombapic.png');
+ 	this.load.audio('clearc','audio/courseclear.mp3');
+	this.load.audio('tps','audio/warp.mp3');
+ 
+ 	this.load.spritesheet('tiles', 'assets/newtiles.png', 16,
+ 					16);
+ 	this.load.spritesheet('goomba', 'assets/goomba.png', 16, 16);
+ 	this.load.spritesheet('mario', 'assets/supermario2.png', 16, 16);
+ 	this.load.spritesheet('coin', 'assets/cointest1.png', 16, 16);
+ 	this.load.spritesheet('pyroguys','assets/shyguy13.png',16,16);
+ 
+ 	this.load.tilemap('level', 'assets/2016178.json', null,
+ 					Phaser.Tilemap.TILED_JSON);
 		},
 
 		create: function() {
